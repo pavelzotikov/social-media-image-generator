9809aca80457f9da577d2b137da28e4a4d4d061c
Pavel Zotikov <pavelzotikov@gmail.com>
2019-01-21 18:49:19 +0300

    Disable normalizeImage

--- src/SocialMediaImageGenerator/Generator.php
+++ src/SocialMediaImageGenerator/Generator.php
@@ -100,7 +100,7 @@ class Generator
             }
         }
 
-        $this->image->normalizeImage();
+        // $this->image->normalizeImage();
         $this->image->unsharpMaskImage(0, 0.5, 1, 0.05);
 
         $this->image->setImageFormat('JPG');
